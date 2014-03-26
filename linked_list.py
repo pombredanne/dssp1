3cc1c59575c289cd39eae5c4dcb2f392f0d23f5d
Okal Otieno <okal@justokal.com>
2014-03-26 22:12:27 +0300

    Add remove method to LinkedList class

--- linked_list.py
+++ linked_list.py
@@ -16,7 +16,7 @@ class LinkedList:
 
     def add(self, value):
         """
-        Adds the provided value to the end of the linked list
+        Adds the provided value to the end of the linked list O(1)
         """
         node = Node(value)
         if not self._head:
@@ -27,4 +27,30 @@ class LinkedList:
             self._tail = node
 
         self.count += 1
+
+    def remove(self, value):
+        """
+        Removes the first occurence of a node which matches the given value O(n)
+        """
+        previous = None
+        current = self._head
+
+        while current:
+            if current.value == value:
+                if previous:
+                    previous.nextNode = current.nextNode
+                    if not current.nextNode:
+                        self._tail = previous
+                else:
+                    self._head = self._head.nextNode
+                    if not self._head:
+                        self._tail = None
+
+                self.count -= 1
+                return true
+
+            previous = current
+            current = current.nextNode
+
+        return false
 
