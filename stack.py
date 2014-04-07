f8f144672abd7455acb23d25808f2e35a8476589
Okal Otieno <okal@justokal.com>
2014-04-08 02:46:36 +0300

    Switch to DoublyLinkedList for Stack.items since push requires "add_last"

--- stack.py
+++ stack.py
@@ -2,7 +2,7 @@ import linked_list
 
 class Stack(object):
     def __init__(self):
-        self.items = linked_list.LinkedList()
+        self.items = linked_list.DoublyLinkedList()
 
     def push(self, value):
         pass
