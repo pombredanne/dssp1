c4aefa33251783625e81942ad790422188cb9bd2
Okal Otieno <okal@justokal.com>
2014-04-08 03:07:02 +0300

    Add method to retrieve depth of stack (get_count)

--- stack.py
+++ stack.py
@@ -19,3 +19,7 @@ class Stack(object):
         tail_value = self.items._tail.value
         self.items.remove_last()
         return tail_value
+
+    def get_count(self):
+        return self.items.count
+
