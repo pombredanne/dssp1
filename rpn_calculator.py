b728f3154adb9a77777f35168845bf854ecd6b45
Okal Otieno <okal@justokal.com>
2014-04-08 03:58:04 +0300

    Add operations to RPN calculator Interesting use of lambdas and a dictionary.

--- rpn_calculator.py
+++ rpn_calculator.py
@@ -1,16 +1,31 @@
 from stack import Stack
 
 def calculate():
+    operators = {
+        "*": lambda left, right: left * right,
+        "-": lambda left, right: left - right,
+        "+": lambda left, right: left + right,
+        "/": lambda left, right: left / right,
+        "%": lambda left, right: left / right,
+    }
+
     calculator_input = raw_input("Enter expression to evaluate: ")
+
     values = Stack()
     tokens = calculator_input.strip().split()
+
     for token in tokens:
         try:
             operand = int(token)
             values.push(operand)
         except ValueError:
             if token in ("+", "-", "*", "/", "%"):
-                operator = parseOperator(token)
+                right = values.pop()
+                left = values.pop()
+                result = operators[token](left, right)
+                values.push(result)
             else:
                 raise ValueError("Unrecognized token")
+
+    return values.pop()
 
